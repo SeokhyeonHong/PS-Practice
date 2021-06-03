93037d1354971f4c103e621372fde3863d03f1f6
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2021-06-03 12:04:19 +0900

    백준 1463 [1로 만들기]

--- baekjoon_1463.cpp
+++ baekjoon_1463.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int count[1000001] = { 0 };
+int answer[1000001] = { 0 };
 int N;
 
 int main()
@@ -10,13 +10,13 @@ int main()
     scanf("%d", &N);
     
     for(int i = 2; i <= N; ++i) {
-        int ith_count = count[i - 1] + 1;
+        int ith_count = answer[i - 1] + 1;
         if(i % 3 == 0)
-            ith_count = min(ith_count, count[i / 3] + 1);
+            ith_count = min(ith_count, answer[i / 3] + 1);
         if(i % 2 == 0)
-            ith_count = min(ith_count, count[i / 2] + 1);
-        count[i] = ith_count;
+            ith_count = min(ith_count, answer[i / 2] + 1);
+        answer[i] = ith_count;
     }
     
-    printf("%d", count[N]);
+    printf("%d", answer[N]);
 }
