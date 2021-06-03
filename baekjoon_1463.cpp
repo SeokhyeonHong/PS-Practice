77d31961f76917062de249254969c8c9ceb7b348
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2021-06-03 12:03:11 +0900

    Update baekjoon_1463.cpp

--- baekjoon_1463.cpp
+++ baekjoon_1463.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-int count[1000001];
+int count[1000001] = { 0 };
 int N;
 
 int main()
 {
     scanf("%d", &N);
     
-    count[1] = 0;
     for(int i = 2; i <= N; ++i) {
         int ith_count = count[i - 1] + 1;
         if(i % 3 == 0)
