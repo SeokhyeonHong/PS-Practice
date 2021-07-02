77d00e64bdf0596994aa7375f084f37cc3587d1f
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2021-07-02 14:41:55 +0900

    백준 1699 [제곱수의 합]
    백준 1699 [제곱수의 합]
    : DP with memoization (faster than recursion)

---
+++ baekjoon_1699_v02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int sumSq[100001];
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; ++i)
+        sumSq[i] = i;
+
+    for(int i = 1; i <= N; ++i)
+        for(int j = 1; j * j <= i; ++j)
+            sumSq[i] = min(sumSq[i], 1 + sumSq[i - j * j]);
+    
+    printf("%d", sumSq[N]);
+}
